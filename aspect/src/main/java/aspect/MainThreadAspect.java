bd39891e7d17166a8283bd279454780b69a64930
yuansui <forgetman@qq.com>
2018-05-31 15:39:47 +0800

    Sync->MainThread Async->SubThread

--- aspect/src/main/java/aspect/MainThreadAspect.java
+++ aspect/src/main/java/aspect/MainThreadAspect.java
@@ -1,7 +1,5 @@
 package aspect;
 
-import android.os.Looper;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,7 +8,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * 采用RxJava异步处理
@@ -19,26 +16,23 @@ import io.reactivex.schedulers.Schedulers;
  * @since 2018/5/11 0011
  */
 @Aspect
-public class AsyncAspect extends BaseAspect {
+public class MainThreadAspect extends BaseAspect {
 
-    @Pointcut("@within(aspect.annotation.Async)||@annotation(aspect.annotation.Async)")
+    @Pointcut("@within(aspect.annotation.MainThread)||@annotation(aspect.annotation.MainThread)")
     public void methodCut() {
     }
 
     @Around(START_SYNTHETIC + "methodCut()")
     public void around(final ProceedingJoinPoint joinPoint) {
         Flowable.create(e -> {
-            Looper.prepare();
             try {
                 joinPoint.proceed();
             } catch (Throwable throwable) {
                 throwable.printStackTrace();
             }
-            Looper.loop();
         }, BackpressureStrategy.BUFFER)
-                .subscribeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe();
-
     }
 }
--- aspect/src/main/java/aspect/SubThreadAspect.java
+++ aspect/src/main/java/aspect/SubThreadAspect.java
@@ -1,5 +1,7 @@
 package aspect;
 
+import android.os.Looper;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,6 +10,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * 采用RxJava异步处理
@@ -16,23 +19,26 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
  * @since 2018/5/11 0011
  */
 @Aspect
-public class SyncAspect extends BaseAspect {
+public class SubThreadAspect extends BaseAspect {
 
-    @Pointcut("@within(aspect.annotation.Sync)||@annotation(aspect.annotation.Sync)")
+    @Pointcut("@within(aspect.annotation.SubThread)||@annotation(aspect.annotation.SubThread)")
     public void methodCut() {
     }
 
     @Around(START_SYNTHETIC + "methodCut()")
     public void around(final ProceedingJoinPoint joinPoint) {
         Flowable.create(e -> {
+            Looper.prepare();
             try {
                 joinPoint.proceed();
             } catch (Throwable throwable) {
                 throwable.printStackTrace();
             }
+            Looper.loop();
         }, BackpressureStrategy.BUFFER)
-                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe();
+
     }
 }
