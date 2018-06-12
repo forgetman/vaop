0ae4835430b00bc5a630c7fb96170afa5e73eb14
yuansui <forgetman@qq.com>
2018-06-12 13:45:36 +0800

    try build plugin in 'jitpack(1)'

--- aspect-plugin/src/main/groovy/plugin/AspectjPlugin.groovy
+++ aspect-plugin/src/main/groovy/plugin/AspectjPlugin.groovy
@@ -1,24 +1,31 @@
 package plugin
 
-import org.aspectj.bridge.MessageHandler
-import org.aspectj.tools.ajc.Main
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.internal.impldep.org.testng.remote.strprotocol.IMessage
 
 class AspectjPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        project.dependencies {
-            implementation 'org.aspectj:aspectjrt:1.9.1'
-            implementation 'io.reactivex.rxjava2:rxandroid:2.0.2'
-        }
+        project.plugins.apply("android-aspectjx")
+
         final def log = project.logger
-        log.error "========================";
-        log.error "Aspectj切片开始编织Class!";
-        log.error "========================";
-        project.android.applicationVariants.all { variant ->
+        log.error "========================"
+        log.error "Aspectj切片开始编织Class!"
+        log.error "========================"
+
+        def hasApp = project.plugins.withType(AppPlugin)
+        def hasLib = project.plugins.withType(LibraryPlugin)
+        final def variants
+        if (hasApp) {
+            variants = project.android.applicationVariants
+        } else {
+            variants = project.android.libraryVariants
+        }
+
+        variants.all { variant ->
             def javaCompile = variant.javaCompile
             javaCompile.doLast {
                 String[] args = ["-showWeaveInfo",
@@ -29,27 +36,6 @@ class AspectjPlugin implements Plugin<Project> {
                                  "-classpath", javaCompile.classpath.asPath,
                                  "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)]
                 log.debug "ajc args: " + Arrays.toString(args)
-
-                MessageHandler handler = new MessageHandler(true);
-                new Main().run(args, handler);
-                for (IMessage message : handler.getMessages(null, true)) {
-                    switch (message.getKind()) {
-                        case IMessage.ABORT:
-                        case IMessage.ERROR:
-                        case IMessage.FAIL:
-                            log.error message.message, message.thrown
-                            break;
-                        case IMessage.WARNING:
-                            log.warn message.message, message.thrown
-                            break;
-                        case IMessage.INFO:
-                            log.info message.message, message.thrown
-                            break;
-                        case IMessage.DEBUG:
-                            log.debug message.message, message.thrown
-                            break;
-                    }
-                }
             }
         }
     }
